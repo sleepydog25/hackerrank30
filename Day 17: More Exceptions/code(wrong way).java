acc76674d2672f01c2b93113fea16cc968bb8e8a
Sleepy_dog <124705401+sleepydog25@users.noreply.github.com>
2024-03-28 10:42:00 +0800

    Update code(wrong way).java

--- Day 17: More Exceptions/code(wrong way).java
+++ Day 17: More Exceptions/code(wrong way).java
@@ -7,36 +7,37 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner s = new Scanner (System.in);
+        
+        //get the number of the test cases
         int T = s.nextInt();
-
+        
+        //calculate the power
         for (int i = 0; i<T;i++){
-            
             int n = s.nextInt();
             int p = s.nextInt();
             Calculator c = new Calculator (n,p);
-            c.countPower();
+            c.countPower();  
             
         }
     }
 }
-
+ 
 class Calculator{
     
-    private int T;
     private int n;
     private int p;
     
+    //setter
     public Calculator(int n, int p){
         this.n = n;
         this.p = p;
     }
-    
+    // calculate power
     public void countPower(){
-        
         if (n>=0 && p>=0)
           System.out.println((int)Math.pow(n,p));
         else if(n<0 || p<0){
             System.out.println("n and p should be non-negative");
-        }
+        }          
     }   
 }
